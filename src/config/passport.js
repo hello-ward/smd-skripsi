7b43f17bf00cfee2dab250e518f2637eb525bc9f
Rizal Hidayat <rizalhidayat180499@gmail.com>
2021-02-05 16:44:34 +0700

    add permissions route

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,18 +1,18 @@
 import passport from 'passport';
 import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
 
-import { UserModel, AdminModel } from '../models';
+import UserModel from '../models/user.model';
 import constants from './constants';
 
-const jwtOpts = {
+const jwtAdminOpts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: constants.JWT_SECRET,
 };
 
-passport.use('user',
-    new JWTStrategy(jwtOpts, async (payload, done) => {
+passport.use('admin',
+    new JWTStrategy(jwtAdminOpts, async (payload, done) => {
         try {
-            const user = await UserModel.findOne({ _id: payload.userId });
+            const user = await UserModel.findOne({ _id: payload.user_id, roles: { $nin: ['superadmin', 'admin'] } });
 
             if (!user) {
                 return done(null, false);
@@ -25,17 +25,17 @@ passport.use('user',
     })
 )
 
-const jwtAdminOpts = {
+const jwtOpts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: constants.JWT_SECRET,
 };
 
-passport.use('admin',
-    new JWTStrategy(jwtAdminOpts, async (payload, done) => {
+passport.use('driver',
+    new JWTStrategy(jwtOpts, async (payload, done) => {
         try {
-            const user = await AdminModel.findOne({ _id: payload.adminId });
+            const user = await UserModel.findOne({ _id: payload.user_id, roles: 'driver' });
 
-            if (!user || !payload.admin) {
+            if (!user) {
                 return done(null, false);
             }
 
@@ -46,49 +46,22 @@ passport.use('admin',
     })
 )
 
-const jwtInterconnectionOpts = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: constants.JWT_SECRET,
-};
-
-passport.use('interconnection',
-    new JWTStrategy(jwtInterconnectionOpts, async (payload, done) => {
+passport.use('marketing',
+    new JWTStrategy(jwtOpts, async (payload, done) => {
         try {
-            if (payload.userId == undefined && payload.adminId == undefined) {
-                return done(null, {
-                    success: false,
-                    message: "Unauthorized"
-                });
+            const user = await UserModel.findOne({ _id: payload.user_id, roles: 'marketing' });
+
+            if (!user) {
+                return done(null, false);
             }
-            if (payload.userId) {
-                const user = await UserModel.findOne({ _id: payload.userId });
-                if (user.banned.status == 1) {
-                    return done(null, {
-                        success: false,
-                        message: "Your are banned"
-                    })
-                }
-                return done(null, {
-                    success: true,
-                    data: user
-                });
-            } else if (payload.adminId) {
-                const admin = await AdminModel.findOne({ _id: payload.adminId });
-                return done(null, {
-                    success: true,
-                    data: admin
-                });
-            }
-            return done(null, {
-                success: false,
-                message: "Unauthorized"
-            });
+
+            return done(null, user);
         } catch (e) {
             return done(e, false);
         }
     })
 )
 
-export const userJwt = passport.authenticate('user', { session: false });
 export const adminJwt = passport.authenticate('admin', { session: false });
-export const interconnectionJwt = passport.authenticate('interconnection', { session: false });
+export const driverJwt = passport.authenticate('driver', { session: false });
+export const marketingJwt = passport.authenticate('marketing', { session: false });
